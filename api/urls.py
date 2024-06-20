01b79abecf3ea5885e9b929e7881787c9fb9f346
minskapp <ignacia.rivas@alumnos.ucn.cl>
2024-06-20 13:43:58 -0400

    feat: add search_worker endpoint

--- api/urls.py
+++ api/urls.py
@@ -21,9 +21,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from api_auth.views import WorkerListView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('workers/', WorkerListView.as_view(), name='worker-list'),
 ]
